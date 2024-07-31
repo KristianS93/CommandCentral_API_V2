2788ebdae485390ee111c37cdc5ba083fe5b7214
Kristian <kristians93@hotmail.com>
2024-07-31 21:09:21 +0200

    add regions for endpoints

--- API/MealPlanner/MealPlannerEndpoints.cs
+++ API/MealPlanner/MealPlannerEndpoints.cs
@@ -5,6 +5,24 @@ public static class MealPlannerEndpoints
     public static WebApplication AddMealPlannerEndpoints(this WebApplication app)
     {
         var mealplanner = app.MapGroup("/mealplanner").WithOpenApi().WithTags("Mealplanner");
+
+        #region Ingredients
+
+        
+
+        #endregion
+
+        #region Meals
+
+        
+
+        #endregion
+
+        #region MealPlan
+
+        
+
+        #endregion
         
         
         
