ab2eca584e422daaa8ff13acc2c3b8eeb31af59b
Kristian <kristians93@hotmail.com>
2024-08-07 21:15:40 +0200

    Fixes #34, change the get meal/id endpoint to such that ingredients does not contain mealid aswell.

--- API/MealPlanner/Models/MealDto.cs
+++ API/MealPlanner/Models/MealDto.cs
@@ -1,3 +1,3 @@
 namespace API.MealPlanner.Models;
 
-public record MealDto(string MealId, string Name, string Description, List<IngredientDto> Ingredients);
+public record MealDto(string MealId, string Name, string Description, List<MealIngredientDto> Ingredients);
